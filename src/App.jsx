43dbe07daf5c7a4161051a0464363cf9383860db
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-09-15 01:27:21 +0530

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Service from './pages/Service'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 const App = () => {
   return (
     <BrowserRouter>
